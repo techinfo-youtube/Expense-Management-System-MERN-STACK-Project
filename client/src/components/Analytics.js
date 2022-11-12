2c2974e2bd40a5730494e515673321e453308664
Rasheed khan <rktechnicalplus@gmail.com>
2022-11-12 13:56:39 +0530

    code-till-Analytics
    complete code till analytics

---
+++ client/src/components/Analytics.js
@@ -0,0 +1,94 @@
+import React from "react";
+import { Progress } from "antd";
+const Analytics = ({ allTransection }) => {
+  // total transaction
+  const totalTransaction = allTransection.length;
+  const totalIncomeTransactions = allTransection.filter(
+    (transaction) => transaction.type === "income"
+  );
+  const totalExpenseTransactions = allTransection.filter(
+    (transaction) => transaction.type === "expense"
+  );
+  const totalIncomePercent =
+    (totalIncomeTransactions.length / totalTransaction) * 100;
+  const totalExpensePercent =
+    (totalExpenseTransactions.length / totalTransaction) * 100;
+
+  //total turnover
+  const totalTurnover = allTransection.reduce(
+    (acc, transaction) => acc + transaction.amount,
+    0
+  );
+  const totalIncomeTurnover = allTransection
+    .filter((transaction) => transaction.type === "income")
+    .reduce((acc, transaction) => acc + transaction.amount, 0);
+
+  const totalExpenseTurnover = allTransection
+    .filter((transaction) => transaction.type === "expense")
+    .reduce((acc, transaction) => acc + transaction.amount, 0);
+
+  const totalIncomeTurnoverPercent =
+    (totalIncomeTurnover / totalTurnover) * 100;
+  const totalExpenseTurnoverPercent =
+    (totalExpenseTurnover / totalTurnover) * 100;
+  return (
+    <>
+      <div className="row m-3">
+        <div className="col-md-4">
+          <div className="card">
+            <div className="card-header">
+              Total Transactions : {totalTransaction}
+            </div>
+            <div className="card-body">
+              <h5 className="text-success">
+                Income : {totalIncomeTransactions.length}
+              </h5>
+              <h5 className="text-danger">
+                Expense : {totalExpenseTransactions.length}
+              </h5>
+              <div>
+                <Progress
+                  type="circle"
+                  strokeColor={"green"}
+                  className="mx-2"
+                  percent={totalIncomePercent.toFixed(0)}
+                />
+                <Progress
+                  type="circle"
+                  strokeColor={"red"}
+                  className="mx-2"
+                  percent={totalExpensePercent.toFixed(0)}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-4">
+          <div className="card">
+            <div className="card-header">Total TurnOver : {totalTurnover}</div>
+            <div className="card-body">
+              <h5 className="text-success">Income : {totalIncomeTurnover}</h5>
+              <h5 className="text-danger">Expense : {totalExpenseTurnover}</h5>
+              <div>
+                <Progress
+                  type="circle"
+                  strokeColor={"green"}
+                  className="mx-2"
+                  percent={totalIncomeTurnoverPercent.toFixed(0)}
+                />
+                <Progress
+                  type="circle"
+                  strokeColor={"red"}
+                  className="mx-2"
+                  percent={totalExpenseTurnoverPercent.toFixed(0)}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Analytics;
